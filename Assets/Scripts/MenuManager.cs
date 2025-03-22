a394aed889d0d76e6e1478a6442d549ee3139385
Vanilla <010thebest010@gmail.com>
2025-03-22 14:23:12 +0100

    git v 0.04
    - Menu gry z opcjami

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
+
+    public GameObject OpcjeMenu;
+    public GameObject MenuOpcje;
     public void Odpal( string nazwaSceny)
     {
         SceneManager.LoadScene(nazwaSceny);
     }
 
+    public void PokazOpcje()
+    {
+        OpcjeMenu.SetActive(true);
+        MenuOpcje.SetActive(false);
+    }
+
+    public void PokazMenu()
+    {
+        OpcjeMenu.SetActive(false);
+        MenuOpcje.SetActive(true);
+    }
+
     public void WyjscieGry()
     {
         Application.Quit();
-        Debug.Log("Gra zosta³a zamkniêta.");
+        Debug.Log("Gra zostaï¿½a zamkniï¿½ta.");
     }
 
 }
